84dc5eacd25c5978adaced4e4a98cc73774e181d
jiibal <aliyueyebee@gmail.com>
2024-10-08 14:32:26 +0100

    main

---
+++ name.js
@@ -0,0 +1,34 @@
+//Addition //
+
+var pink=15;
+var YEllow=25;
+var Ash=5;
+var color= pink+ YEllow + Ash;
+document.write(color);
+document.write("<br>");
+
+
+//Subtarction//
+
+var black=40;
+var green=20;
+var purple=14;
+var color= black- green - purple;
+document.write(color);
+document.write("<br>");
+
+//Division//
+
+const book=80;
+const biro=8;
+const items= book / biro;
+document.write(items);
+document.write("<br>");
+
+//Multiplication//
+
+const tyres=10;
+const wheel=10;
+const parts= tyres * wheel;
+document.write(parts);
+document.write("<br>");
